2d22a8850dd69c84565b5b8c17db03ec70a34533
RyanMorris95 <morrisryanmorris@gmail.com>
2018-03-11 18:29:44 -0700

    SVO Point Cloud to Octomap

---
+++ src/roguetwo_navigation/src/markers_to_pcl_node.cpp
@@ -0,0 +1,70 @@
+// #include "markers_to_pcl_node.hpp"
+
+// MarkersToPCLNode::MarkersToPCLNode(int argc, char** argv)
+// {
+// 	ros::init(argc, argv, "markers_to_pcl");
+// 	pcl_pub = n.advertise<sensor_msgs::PointCloud2>("point_cloud", 10);
+// 	markers_sub = n.subscribe("/svo/points_array", 10, MarkersToPCLNode::markers_to_pcl);
+// }
+
+// void MarkersToPCLNode::markers_to_pcl(const visualization_msgs::MarkerArray marker_array)
+// {
+// 	std::cout << "Here1" << std::endl;
+// 	MarkersToPCLNode::publish_pcl();
+// }
+
+// void MarkersToPCLNode::publish_pcl()
+// {
+// 	std::cout << "Here2" << std::endl;
+// }
+
+
+// int main(int argc, char** argv)
+// {
+// 	// setup up node, publisher, and subscriber
+// 	MarkersToPCLNode markers_to_pcl = MarkersToPCLNode(argc, argv);
+// }
+
+#include <ros/ros.h>
+#include <visualization_msgs/Marker.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <std_msgs/Header.h>
+#include <pcl_ros/point_cloud.h>
+#include <geometry_msgs/Point.h>
+#include <iostream>
+
+typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
+
+ros::Publisher pub;
+
+void markers_cb(const visualization_msgs::Marker& marker)
+{
+	std::vector<geometry_msgs::Point> points;
+	sensor_msgs::PointCloud2 pcl_msg;
+	PointCloud::Ptr pcl (new PointCloud);
+	for (std::vector<int>::size_type i = 0; i != marker.points.size(); i++)
+	{
+		pcl::PointXYZ point = pcl::PointXYZ(marker.points[i].z, -1*marker.points[i].x, -1*marker.points[i].y);
+		pcl->points.push_back(point);
+	}
+	//pcl.sensor_origin_ = 
+	//std::cout << pcl << std::endl
+	pcl::toROSMsg(*pcl, pcl_msg);
+	std_msgs::Header pcl_header;
+	pcl_header.frame_id = "camera_stereo_left";
+	pcl_msg.header = pcl_header;
+	pcl_msg.is_dense = false;
+	pub.publish(pcl_msg);
+}
+
+int main(int argc, char** argv)
+{
+	ros::init(argc, argv, "markers_to_pcl");
+	ros::NodeHandle nh;
+
+	ros::Subscriber sub = nh.subscribe("/svo/points", 1, markers_cb);
+
+	pub = nh.advertise<sensor_msgs::PointCloud2>("cloud_in", 1);
+
+	ros::spin();
+}
