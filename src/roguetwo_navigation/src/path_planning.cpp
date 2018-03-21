d69cd73283b1f210e517c0ed9fbe0d956f5eb6fb
RyanMorris95 <morrisryanmorris@gmail.com>
2018-03-21 12:27:14 +1030

    Removed C++11 from path planning

--- src/roguetwo_navigation/src/path_planning.cpp
+++ src/roguetwo_navigation/src/path_planning.cpp
@@ -17,19 +17,22 @@ bool PathPlanning::isStateValid(
 	const ob::State *state
 	)
 {
-	// cast the abstract state type to the type we expect
-	const auto *se2state = state->as<ob::SE2StateSpace::StateType>();
+    //    ob::ScopedState<ob::SE2StateSpace>
+    // cast the abstract state type to the type we expect
+    const ob::SE2StateSpace::StateType *se2state = state->as<ob::SE2StateSpace::StateType>();
 
-	// extract the first component of the state and cast it to what we expect
-	const auto *pos = se2state->as<ob::RealVectorStateSpace::StateType>(0);
+    // extract the first component of the state and cast it to what we expect
+    const ob::RealVectorStateSpace::StateType *pos = se2state->as<ob::RealVectorStateSpace::StateType>(0);
 
-	// extract the second component of the state and cast it to what we expect
-	const auto *rot = se2state->as<ob::SO2StateSpace::StateType>(1);
+    // extract the second component of the state and cast it to what we expect
+    const ob::SO2StateSpace::StateType *rot = se2state->as<ob::SO2StateSpace::StateType>(1);
 
-	// check validity of state defined by pos & rot
+    // check validity of state defined by pos & rot
 
-	// return a value that is always true but uses the two variables we define
-	return si->satisfiesBounds(state) && (const void*)rot != (const void*)pos;
+
+    // return a value that is always true but uses the two variables we define, so we avoid compiler warnings
+    return si->satisfiesBounds(state) && (const void*)rot != (const void*)pos;
+
 }
 
 
@@ -40,7 +43,7 @@ void PathPlanning::propagate(
 	ob::State *result
 	)
 {
-	const auto* se2state = state->as<ob::SE2StateSpace::StateType>();
+	const ob::SE2StateSpace::StateType* se2state = state->as<ob::SE2StateSpace::StateType>();
 	const double* pos = se2state->as<ob::RealVectorStateSpace::StateType>(0)->values;
 	const double rot = se2state->as<ob::SO2StateSpace::StateType>(1)->value;
 	const double* ctrl = control->as<oc::RealVectorControlSpace::ControlType>()->values;
@@ -56,47 +59,43 @@ void PathPlanning::propagate(
 void PathPlanning::plan(const ros::TimerEvent& event)
 {
     // construct the state space we are planning in
-    auto space(std::make_shared<ob::SE2StateSpace>());
+    ob::StateSpacePtr space(new ob::SE2StateSpace());
 
     // set the bounds for the R^2 part of SE(2)
     ob::RealVectorBounds bounds(2);
     bounds.setLow(-1);
     bounds.setHigh(1);
 
-    space->setBounds(bounds);
+    space->as<ob::SE2StateSpace>()->setBounds(bounds);
 
     // create a control space
-    auto cspace(std::make_shared<oc::RealVectorControlSpace>(space, 2));
+    oc::ControlSpacePtr cspace(new oc::RealVectorControlSpace(space, 2));
 
     // set the bounds for the control space
     ob::RealVectorBounds cbounds(2);
     cbounds.setLow(-0.3);
     cbounds.setHigh(0.3);
 
-    cspace->setBounds(cbounds);
+    cspace->as<oc::RealVectorControlSpace>()->setBounds(cbounds);
 
     // define a simple setup class
     oc::SimpleSetup ss(cspace);
 
     // set the state propagation routine
-    ss.setStatePropagator(PathPlanning::propagate);
+    ss.setStatePropagator(boost::bind(&propagate, _1, _2, _3, _4));
 
     // set state validity checking for this space
-    ss.setStateValidityChecker(
-        [&ss](const ob::State *state) { return PathPlanning::isStateValid(ss.getSpaceInformation().get(), state); });
+    ss.setStateValidityChecker(boost::bind(&isStateValid, ss.getSpaceInformation().get(), _1));
 
     // create a start state
     ob::ScopedState<ob::SE2StateSpace> start(space);
-//    start->setX(se2_state[0]);
-//    start->setY(se2_state[1]);
-//    start->setYaw(se2_state[2]);
-    start->setX(0);
-    start->setY(0);
-    start->setYaw(0);
+    start->setX(-0.5);
+    start->setY(0.0);
+    start->setYaw(0.0);
 
     // create a  goal state; use the hard way to set the elements
     ob::ScopedState<ob::SE2StateSpace> goal(space);
-    (*goal)[0]->as<ob::RealVectorStateSpace::StateType>()->values[0] = 10.0;
+    (*goal)[0]->as<ob::RealVectorStateSpace::StateType>()->values[0] = 0.0;
     (*goal)[0]->as<ob::RealVectorStateSpace::StateType>()->values[1] = 0.5;
     (*goal)[1]->as<ob::SO2StateSpace::StateType>()->value = 0.0;
 
@@ -104,17 +103,17 @@ void PathPlanning::plan(const ros::TimerEvent& event)
     // set the start and goal states
     ss.setStartAndGoalStates(start, goal, 0.05);
 
-    // ss.setPlanner(std::make_shared<oc::PDST>(ss.getSpaceInformation()));
+    // ss.setPlanner(ob::PlannerPtr(new oc::PDST(ss.getSpaceInformation())));
     // ss.getSpaceInformation()->setMinMaxControlDuration(1,100);
     // attempt to solve the problem within one second of planning time
-    ob::PlannerStatus solved = ss.solve(0.1);
+    ob::PlannerStatus solved = ss.solve(10.0);
 
     if (solved)
     {
         std::cout << "Found solution:" << std::endl;
         // print the path to screen
 
-        //ss.getSolutionPath().printAsMatrix(std::cout);
+        ss.getSolutionPath().printAsMatrix(std::cout);
     }
     else
         std::cout << "No solution found" << std::endl;
@@ -128,7 +127,7 @@ void PathPlanning::plan(const ros::TimerEvent& event)
     std::vector<oc::Control*>& controls = solution_path.getControls();
     std::vector<double>& control_durations = solution_path.getControlDurations();
 
-    for (auto i = 0; i != states.size(); i++)
+    for (int i = 0; i != states.size(); i++)
     {
         double x = states[i]->as<ob::SE2StateSpace::StateType>()->getX();
         double y = states[i]->as<ob::SE2StateSpace::StateType>()->getY();
@@ -179,7 +178,6 @@ int main(int argc, char **argv)
 		ros::Duration(0.5),
 		&PathPlanning::plan,
 		&path_planning);
-//    path_planning.plan();
 
 
 	ros::spin();
