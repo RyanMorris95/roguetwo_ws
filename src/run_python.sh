5f244e6461347aea042128c9160ad12af857532a
RyanMorris95 <morrisryanmorris@gmail.com>
2018-04-15 17:39:06 -0700

    more bs

--- src/run_python.sh
+++ src/run_python.sh
@@ -3,7 +3,7 @@ python roguetwo_hardware/scripts/joy_to_ackermann.py &
 echo "started: joy_to_ackermann.py"
 python roguetwo_hardware/scripts/motor_servo_comm.py &
 echo "started: motor_servo_comm.py"
-# python roguetwo_hardware/scripts/gps_comm.py &
-# echo "started: gps_comm.py"
+python roguetwo_hardware/scripts/gps_comm.py &
+echo "started: gps_comm.py"
 python roguetwo_perception/scripts/gps_imu_to_se2.py &
 echo "started: gps_imu_to_se2.py"
