lidar mapping fix, encoder covariance

src/roguetwo_navigation/scripts/plot_odometry.py
@@ -58,7 +58,6 @@ class PlotSE2(object):
                     x = height * occupancy_grid_msg.info.resolution + occupancy_grid_msg.info.resolution / 2.
                     x -= occupancy_grid_msg.info.origin.position.x
                     self.obstacles.append([float(y), float(x)])
-                    print (y, x)
 
         self.obstacles = np.array(self.obstacles)
 
@@ -90,8 +89,6 @@ class PlotSE2(object):
         self.vel_y = (self.se2_y[-1] - self.se2_y[-2]) / dt
         self.yaw = self.get_yaw(odometry)
         self.speed = self.vel_x * math.cos(self.yaw)
-        print (self.yaw)
-
 
     def update_se2_ekf(self, odometry):
         self.se2_x_ekf.append(odometry.pose.pose.position.x)
@@ -109,7 +106,7 @@ class PlotSE2(object):
             plt.scatter(self.se2_x_ekf, self.se2_y_ekf, c='green', label='EKF', s=2)
             plt.scatter(self.x_states, self.y_states, c='blue', label='Path', s=1, alpha=0.3)
             if self.obstacles.all() and self.obstacles.shape[0] > 0:
-                plt.scatter(self.obstacles[:, 0], self.obstacles[:, 1])
+                plt.scatter(self.obstacles[:, 0], self.obstacles[:, 1], s=0.5)
             plt.arrow(self.se2_x[-1], self.se2_y[-1],
                         0.5 * math.cos(self.yaw),
                         0.5 * math.sin(self.yaw),
@@ -123,7 +120,7 @@ class PlotSE2(object):
                         head_length=0.05,
                         fc='k', ec='k')
             vel_str = "Vx: " + str(round(self.vel_x, 2)) + "  Vy: " + str(round(self.vel_y, 2)) + "  Vx2: " + str(round(self.vel_x2, 2)) + "  Vy2: " + str(round(self.vel_y2, 2))
-            speed_str = "Speed: " + str(round(self.speed, 2)) + "  Speed2: " + str(round(self.speed2, 2))
+            speed_str = "Yaw: " + str(round(self.yaw)) + "  Speed: " + str(round(self.speed, 2)) + "  Speed2: " + str(round(self.speed2, 2))
             #plt.title(vel_str)
             plt.title(speed_str)
 